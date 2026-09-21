44129a56ced3b732d7b2515387d01a770f391527
sixelasacul <pro@sixe.la>
2026-09-21 11:14:08 +0200

    Migrating to set 18 (bit late)

--- src/lib/api/tft.ts
+++ src/lib/api/tft.ts
@@ -40,7 +40,7 @@ async function getLatestTftSetData() {
   return okAsync(data);
 }
 
-export const CURRENT_SET_NUMBER = 17;
+export const CURRENT_SET_NUMBER = 18;
 
 const playableChampionsType = playableChampionType.array();
 export async function getAllDataForCurrentSet() {
